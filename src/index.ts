3b3a11e05d0271148b44fec4e598de8f32d24b8d
ulrikah <ulrikhalmoy@gmail.com>
2020-08-10 10:46:56 +0200

    Hello world in THREE.js

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,4 @@
-import * as _ from 'lodash';
-import printMe from './print.js';
+import main from './hello';
 
-function component() {
-    const element = document.createElement('div');
-    const btn = document.createElement('button');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    btn.innerHTML = "Click me";
-    btn.onclick = printMe;
 
-    element.appendChild(btn);
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+main()
