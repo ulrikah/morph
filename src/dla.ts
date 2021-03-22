f1156b0fbe89f032a1ac3ac6460627464578eec6
ulrikah <ulrikhalmoy@gmail.com>
2021-03-22 12:31:01 +0100

    Re-use materials and geometry for all walkers

--- src/dla.ts
+++ src/dla.ts
@@ -1,76 +1,82 @@
-import * as THREE from 'three';
-import createCanvas from './utils/createCanvas'
-import Walker from './dla/Walker';
-import CameraControls from 'camera-controls';
+import * as THREE from "three";
+import createCanvas from "./utils/createCanvas";
+import Walker from "./dla/Walker";
+import CameraControls from "camera-controls";
+import { Color, MeshPhongMaterial, SphereGeometry } from "three";
 
 CameraControls.install({ THREE: THREE });
 
+const N_WALKERS = 1000;
+
 const main = () => {
-  const canvas = createCanvas(window.innerWidth, window.innerHeight)
-  const renderer = new THREE.WebGLRenderer({ canvas })
+    const canvas = createCanvas(window.innerWidth, window.innerHeight);
+    const renderer = new THREE.WebGLRenderer({ canvas });
 
-  const fov = 75
-  const aspect = 2 // the canvas default
-  const near = 0.1
-  const camera = new THREE.PerspectiveCamera(fov, aspect, near)
-  camera.position.z = 8
-  camera.position.y = 0
+    const fov = 75;
+    const aspect = 2; // the canvas default
+    const near = 0.1;
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near);
+    camera.position.z = 8;
+    camera.position.y = 0;
 
-  const clock = new THREE.Clock()
-  const cameraControls = new CameraControls(camera, renderer.domElement);
+    const clock = new THREE.Clock();
+    const cameraControls = new CameraControls(camera, renderer.domElement);
 
-  const scene = new THREE.Scene()
+    const scene = new THREE.Scene();
+    scene.background = new Color("white");
 
-  const color = 0xFFFFFF
-  const intensity = 1
-  const light = new THREE.DirectionalLight(color, intensity)
-  light.position.set(-1, 2, 4)
+    const color = 0xffffff;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
 
-  scene.add(light)
+    scene.add(light);
 
-  const speed = 0.04;
-  const minDistThreshold = 3;
-  const branch: Walker[] = [new Walker(speed, new THREE.Vector3(0, 0, 0))]
-  const walkers : Walker[] = []
-  for (let i = 0; i < 300; i++) {
-    walkers.push(new Walker(speed))
-  }
-  walkers.map(walker => scene.add(walker.body))
+    const speed = 0.02;
+    const minDistThreshold = 3;
 
-  let doRender = true;
-  const render = (time : number) => {
-    const delta = clock.getDelta();
-    cameraControls.update(delta);
+    const branch: Walker[] = [new Walker(speed, new THREE.Vector3(0, 0, 0))];
+    const walkers: Walker[] = [];
+    for (let i = 0; i < N_WALKERS; i++) {
+        walkers.push(new Walker(speed));
+    }
+    walkers.map((walker) => scene.add(walker.body));
 
-    renderer.render(scene, camera)
-    requestAnimationFrame(render)
-    if (!doRender) { return }
+    let doRender = true;
+    const render = (time: number) => {
+        const delta = clock.getDelta();
+        cameraControls.update(delta);
 
-    walkers
-      .filter(walker => walker.active)
-      .map((walker) => {
-        walker.walk()
-        if (walker.minDistToBranch < minDistThreshold) {
-          branch
-            .forEach(targetWalker => {
-              if (walker.collision(targetWalker)) {
-                branch.push(walker.setInactive());
-              }
-            })
-        } else {
-          walker.minDistToBranch -= walker.speed;
+        renderer.render(scene, camera);
+        requestAnimationFrame(render);
+        if (!doRender) {
+            return;
         }
-      })
-  }
-  const pauseOnSpaceDown = () => {
-    document.addEventListener('keydown', (event: KeyboardEvent) => {
-      if ([" ", "Spacebar"].includes(event.key)) {
-        doRender = !doRender;
-      }
-    })
-  }
-  pauseOnSpaceDown()
-  requestAnimationFrame(render)
-}
 
-export default main
+        walkers
+            .filter((walker) => walker.active)
+            .map((walker) => {
+                walker.walk();
+                if (walker.minDistToBranch < minDistThreshold) {
+                    branch.forEach((targetWalker) => {
+                        if (walker.collision(targetWalker)) {
+                            branch.push(walker.setInactive());
+                        }
+                    });
+                } else {
+                    walker.minDistToBranch -= walker.speed;
+                }
+            });
+    };
+    const pauseOnSpaceDown = () => {
+        document.addEventListener("keydown", (event: KeyboardEvent) => {
+            if ([" ", "Spacebar"].includes(event.key)) {
+                doRender = !doRender;
+            }
+        });
+    };
+    pauseOnSpaceDown();
+    requestAnimationFrame(render);
+};
+
+export default main;
--- src/dla/Walker.ts
+++ src/dla/Walker.ts
@@ -1,48 +1,58 @@
 // eslint-disable-next-line no-unused-vars
-import { Vector3, Mesh, SphereGeometry, MeshPhongMaterial, Color } from "three";
+import { Vector3, Mesh, SphereGeometry, MeshPhongMaterial } from "three";
 import { random } from "../utils/random";
+
+const walkerRadius = 0.1;
+const walkerGeometry = new SphereGeometry(walkerRadius, 32, 32);
+const walkerActiveMaterial = new MeshPhongMaterial({ color: 0xff22ee });
+const walkerInactiveMaterial = new MeshPhongMaterial({ color: 0x22eeff });
 
 class Walker {
     position: Vector3;
-    radius: number;
     speed: number;
     body: Mesh<SphereGeometry, MeshPhongMaterial>;
     active: boolean;
     minDistToBranch: number;
-    constructor (speed = 0.01, position = new Vector3(random(-2, 2), random(-2, 2), random(-2, 2)), radius = 0.1) {
-      const geometry = new SphereGeometry(radius, 32, 32)
-      const material = new MeshPhongMaterial({ color: 0xff22ee })
-      this.body = new Mesh(geometry, material)
-      this.body.position.x = position.x
-      this.body.position.y = position.y
-      this.body.position.z = position.z
-      this.speed = speed;
-      this.active = true;
-      this.minDistToBranch = 0; // used to avoid checking collisions for every frame
+    constructor(
+        speed = 0.01,
+        position = new Vector3(random(-2, 2), random(-2, 2), random(-2, 2))
+    ) {
+        this.body = new Mesh(walkerGeometry, walkerActiveMaterial);
+        this.body.position.x = position.x;
+        this.body.position.y = position.y;
+        this.body.position.z = position.z;
+        this.speed = speed;
+        this.active = true;
+        this.minDistToBranch = 0; // used to avoid checking collisions for every frame
     }
 
     walk = () => {
-      this.body.position.x += random(-this.speed, this.speed)
-      this.body.position.y += random(-this.speed, this.speed)
-      this.body.position.z += random(-this.speed, this.speed)
-    }
+        this.body.position.x += random(-this.speed, this.speed);
+        this.body.position.y += random(-this.speed, this.speed);
+        this.body.position.z += random(-this.speed, this.speed);
+    };
 
-    collision = (target : Walker) => {
-      const d = this.body.position.distanceTo(target.body.position)
-      const r = this.body.geometry.parameters.radius + target.body.geometry.parameters.radius + this.speed + target.speed
-      if (d < r) {
-        return true
-      } else {
-        this.minDistToBranch = d > this.minDistToBranch ? d : this.minDistToBranch
-      }
-      return false
-    }
+    collision = (target: Walker) => {
+        const d = this.body.position.distanceTo(target.body.position);
+        const r =
+            this.body.geometry.parameters.radius +
+            target.body.geometry.parameters.radius +
+            this.speed +
+            target.speed;
+        if (d < r) {
+            return true;
+        } else {
+            this.minDistToBranch =
+                d > this.minDistToBranch ? d : this.minDistToBranch;
+        }
+        return false;
+    };
 
     setInactive = () => {
-      this.active = false;
-      this.body.material.color = new Color(0x22eeff)
-      return this;
-    }
+        this.active = false;
+        this.body.material = walkerInactiveMaterial;
+        return this;
+    };
 }
 
 export default Walker;
