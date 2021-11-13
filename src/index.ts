69363d44b348f9f2210a882571ab303b953b4443
ulrikah <ulrikhalmoy@gmail.com>
2021-11-13 13:44:13 +0100

    Bundle with Vite instead of Webpack

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
-import "./style.css";
 // import main from "./dla/dla";
 // import differentialGrowth from "./diff-growth/dg";
 import differentialGrowth from "./diff-growth/dg2d";
-
-
-
 
 // main();
 differentialGrowth();
