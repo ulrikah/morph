a8b323a3fc0d86ef6fb50250bda5aa08d64f08af
Ulrik Antoniussen Halmøy <ulrikhalmoy@gmail.com>
2021-12-21 18:10:34 +0100

    Refactor SVG file uploading to use callback function as argument

--- src/io.ts
+++ src/io.ts
@@ -1,13 +1,11 @@
 import paper from "paper";
 import createCanvas from "./utils/createCanvas";
 
-const svgElementFromString = (svgString: string): SVGElement => {
+export const svgElementFromString = (svgString: string): SVGElement => {
     let temp = document.createElement("template");
     temp.innerHTML = svgString.trim();
     return temp.content.firstChild as SVGElement;
 };
-
-export const exportSvg = (item: paper.Item) => item.exportSVG;
 
 export const getDownloadableLink = (uri: string): HTMLAnchorElement => {
     const link = document.createElement("a");
@@ -16,15 +14,9 @@ export const getDownloadableLink = (uri: string): HTMLAnchorElement => {
     return link;
 };
 
-export const fileUploader = (): HTMLElement => {
-    const canvas = createCanvas(
-        window.innerWidth / 2,
-        window.innerHeight / 2,
-        "test"
-    );
-    canvas.setAttribute("style", "border: 1px solid black");
-    paper.setup(canvas);
-
+export const fileUploader = (
+    onFilesUploaded: (files: File[]) => void
+): HTMLElement => {
     const inputField = document.createElement("input");
     inputField.setAttribute("type", "file");
 
@@ -32,20 +24,7 @@ export const fileUploader = (): HTMLElement => {
     inputField.onchange = (event: any) => {
         if (event.target && event.target.files.length > 0) {
             inputField.files = event.target.files;
-            (Array.from(event.target.files) as File[]).forEach((file: File) => {
-                file.text().then((content) => {
-                    const item = new paper.Item().importSVG(
-                        svgElementFromString(content),
-                        {
-                            onload: (_: paper.Item) =>
-                                console.log("Imported SVG", content),
-                        }
-                    );
-                    item.strokeColor = new paper.Color("black");
-                    paper.project.activeLayer.addChild(item);
-                    paper.view.update();
-                });
-            });
+            onFilesUploaded(Array.from(event.target.files) as File[]);
         } else {
             console.log("No files in input field");
         }
@@ -54,5 +33,21 @@ export const fileUploader = (): HTMLElement => {
     return inputField;
 };
 
-export const demo = (): HTMLElement =>
-    document.body.appendChild(fileUploader());
+export const demo = (): HTMLElement => {
+    const addSvgFilesToProject = (files: File[]) =>
+        files.forEach((file: File) => {
+            file.text().then((content) => {
+                const item = new paper.Item().importSVG(
+                    svgElementFromString(content),
+                    {
+                        onload: (_: paper.Item) =>
+                            console.log("Imported SVG", content),
+                    }
+                );
+                item.strokeColor = new paper.Color("black");
+                paper.project.activeLayer.addChild(item);
+                paper.view.update();
+            });
+        });
+    return document.body.appendChild(fileUploader(addSvgFilesToProject));
+};
